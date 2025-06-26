4b34ffebd945702034a2c0f3ae15e0c245f8a7c5
stewicca <stewicalf@gmail.com>
2025-06-26 15:36:47 +0700

    initial commit

---
+++ src/main/java/com/wreccy/keuanganku/KeuanganKuApplication.java
@@ -0,0 +1,13 @@
+package com.wreccy.keuanganku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeuanganKuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeuanganKuApplication.class, args);
+	}
+
+}
